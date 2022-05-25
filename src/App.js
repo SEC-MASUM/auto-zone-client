975371900d493ca8bf60bc57ca7326b5afce66c8
SEC-MASUM <masum.jess42@gmail.com>
2022-05-25 20:15:37 +0600

    feat : edit Purchase route to /purchase/:id

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
-          path="/purchase"
+          path="/purchase/:id"
           element={
             <RequireAuth>
               <Purchase />
