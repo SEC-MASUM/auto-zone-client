8ad9c4944b4910e44c8e6291605b7e0783452819
SEC-MASUM <masum.jess42@gmail.com>
2022-05-27 00:00:06 +0600

    feat : implement Ship Now button functionality

--- src/Pages/Dashboard/ManageOrders/OrderRow/OrderRow.js
+++ src/Pages/Dashboard/ManageOrders/OrderRow/OrderRow.js
@@ -2,6 +2,24 @@ import React from "react";
 
 const OrderRow = ({ order, index, refetch }) => {
   const { _id, email, name, orderQty, price, totalPrice, status } = order;
+  const handleShipNow = (id) => {
+    const orderUpdate = {
+      status: "shipped",
+      transactionId: order?.transactionId,
+    };
+    fetch(`http://localhost:5000/payment/order/${_id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify(orderUpdate),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        refetch();
+      });
+  };
   return (
     <tr className="text-center">
       <th>{index + 1}</th>
@@ -16,7 +34,12 @@ const OrderRow = ({ order, index, refetch }) => {
       </td>
       <td>
         {status === "paid" && (
-          <button className="btn btn-sm btn-success">Ship Now</button>
+          <button
+            onClick={() => handleShipNow(_id)}
+            className="btn btn-sm btn-success"
+          >
+            Ship Now
+          </button>
         )}
         {status !== "paid" && (
           <button disabled className="btn btn-sm btn-success">
@@ -25,7 +48,7 @@ const OrderRow = ({ order, index, refetch }) => {
         )}
       </td>
       <td>
-        {status === "paid" && (
+        {(status === "paid" || status === "shipped") && (
           <p>
             Transaction ID: <br />
             <span className="text-orange-600 font-medium">
