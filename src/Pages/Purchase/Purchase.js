c527ed88409c0d0e8bf836d58430cea4f6ba4df6
SEC-MASUM <masum.jess42@gmail.com>
2022-05-26 08:19:19 +0600

    refactor : change style and , post different data

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -3,8 +3,8 @@ import { IoMdSpeedometer } from "react-icons/io";
 import { AiFillPieChart } from "react-icons/ai";
 import Navbar from "../Shared/Navbar/Navbar";
 import Footer from "../Shared/Footer/Footer";
-import Hero from "./Hero";
-import { useParams } from "react-router-dom";
+import Hero from "../../Components/Hero/Hero"
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import axios from "axios";
 import Loading from "../Shared/Loading/Loading";
@@ -13,8 +13,9 @@ import auth from "../../Firebase/Firebase.init";
 import { toast } from "react-toastify";
 
 const Purchase = () => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const { id } = useParams();
+  const navigate = useNavigate();
   // const [orderQty, setOrderQty] = useState(0);
 
   const {
@@ -24,7 +25,7 @@ const Purchase = () => {
   } = useQuery(["product", id], () =>
     axios.get(`http://localhost:5000/product/${id}`)
   );
-  if (isLoading) {
+  if (isLoading || loading) {
     return <Loading />;
   }
   const { _id, name, description, minOrder, availableQty, price, image } =
@@ -39,16 +40,32 @@ const Purchase = () => {
     event.preventDefault();
     const quantity = event.target.quantity.value;
     const totalPrice = quantity * price;
+    const phone = event.target.phone.value;
+    const country = event.target.country.value;
+    const addressLine = event.target.addressLine.value;
+    const state = event.target.state.value;
+    const city = event.target.city.value;
+    const zip = event.target.zip.value;
     console.log(totalPrice);
     const order = {
       email: user.email,
       name: name,
+      phone,
+      address: {
+        country,
+        addressLine,
+        state,
+        city,
+        zip,
+      },
       productId: _id,
       orderQty: quantity,
-      price: price,
-      totalPrice: totalPrice,
+      price,
+      totalPrice,
+      image,
       status: "unpaid",
     };
+    console.log(order);
     fetch("http://localhost:5000/order", {
       method: "POST",
       headers: {
@@ -63,6 +80,7 @@ const Purchase = () => {
         if (inserted.insertedId) {
           toast.success("Order placed successfully");
           event.target.reset();
+          navigate(`/payment/${inserted.insertedId}`);
         } else {
           toast.error("Failed to place order");
         }
@@ -71,7 +89,7 @@ const Purchase = () => {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero>Purchase</Hero>
       <div className="container mx-auto my-12">
         <div className="bg-gray-100 shadow-lg  rounded-3xl p-5">
           <div className="flex-none grid sm:grid-cols-1 md:grid-cols-2 gap-5">
@@ -100,14 +118,12 @@ const Purchase = () => {
                   <p className="">Stock: {availableQty} pieces Available</p>
                 </div>
               </div>
-              <div>
-                <h3 className="text-left text-2xl font-semibold text-primary">
-                  Price : ${price}
-                </h3>
-              </div>
-              <div className="flex p-4 pb-2 border-t border-gray-200 "></div>
               <form onSubmit={handleSubmit}>
-                <div className="flex space-x-3 text-sm font-medium">
+                {/* ----------Price and Order Qty---------- */}
+                <div className="flex justify-between items-center">
+                  <h3 className="text-left text-2xl font-semibold text-primary">
+                    Price : ${price}
+                  </h3>
                   <div className="bg-white px-4 py-2 shadow-sm tracking-wider border text-gray-600 rounded-full hover:bg-gray-100 inline-flex items-center space-x-2 ">
                     <span className="font-medium">Order Qty</span>
                     <input
@@ -121,8 +137,114 @@ const Purchase = () => {
                       className="input input-sm rounded-full font-medium bg-accent text-lg text-secondary max-w-xs "
                     />
                   </div>
+                </div>
+                <div className="flex p-4 pb-2 border-t border-gray-200 "></div>
+                {/* ---------------Address-------- */}
+                <div className="flex flex-col  justify-items-stretch ">
+                  <h1>Shipping Information</h1>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5">
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">Name</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        value={user.displayName}
+                        className="input input-bordered w-full max-w-sm"
+                        disabled
+                        required
+                      />
+                    </div>
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">Email</span>
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={user.email}
+                        className="input input-bordered w-full max-w-sm"
+                        disabled
+                        required
+                      />
+                    </div>
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">Phone</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="phone"
+                        placeholder="Phone"
+                        className="input input-bordered w-full max-w-sm"
+                        required
+                      />
+                    </div>
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">Country</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="country"
+                        placeholder="Country"
+                        className="input input-bordered w-full max-w-sm"
+                        required
+                      />
+                    </div>
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">Address Line</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="addressLine"
+                        placeholder="Address Line"
+                        className="input input-bordered w-full max-w-sm"
+                      />
+                    </div>
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">State</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="state"
+                        placeholder="State"
+                        className="input input-bordered w-full max-w-sm"
+                      />
+                    </div>
 
-                  <button className="flex items-center btn btn-primary px-5 shadow-sm tracking-wider text-white font-medium rounded-full hover:bg-red-600">
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">City</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="city"
+                        placeholder="City"
+                        className="input input-bordered w-full max-w-sm"
+                        required
+                      />
+                    </div>
+                    <div className="form-control mx-auto w-full max-w-sm">
+                      <label className="label">
+                        <span className="label-text">Zip</span>
+                      </label>
+                      <input
+                        type="text"
+                        name="zip"
+                        placeholder="Zip"
+                        className="input input-bordered w-full max-w-sm"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/* ----------------Submit Button-------------- */}
+                <div className="flex justify-center text-sm font-medium pt-3">
+                  <button className="w-full max-w-sm flex items-center btn btn-primary px-5 shadow-sm tracking-wider text-white font-medium rounded-full hover:bg-red-600">
                     Order
                   </button>
                 </div>
