4bb6aaefd810a2e89f1a580ac267c581e1f6a393
SEC-MASUM <masum.jess42@gmail.com>
2022-05-26 23:17:16 +0600

    style : remove App class

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import Payment from "./Pages/Payment/Payment";
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
