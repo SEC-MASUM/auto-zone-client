d1b7161d970292b348d8111cb8be32d526641393
SEC-MASUM <masum.jess42@gmail.com>
2022-05-27 16:11:06 +0600

    feat : create useAdmin Hook to load user role admin or not

---
+++ src/Hooks/useAdmin.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useAdmin = (user) => {
+  const [admin, setAdmin] = useState(false);
+  const [adminLoading, setAdminLoading] = useState(true);
+  useEffect(() => {
+    const email = user?.email;
+    if (email) {
+      const url = `http://localhost:5000/user/admin/${email}`;
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setAdmin(data.admin);
+          setAdminLoading(false);
+        });
+    }
+  }, [user]);
+
+  return [admin, adminLoading];
+};
+
+export default useAdmin;
