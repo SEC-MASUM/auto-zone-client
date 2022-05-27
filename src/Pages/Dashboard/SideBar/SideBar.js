bf12e9009034365427222d16fc513642b2d34636
SEC-MASUM <masum.jess42@gmail.com>
2022-05-27 16:17:24 +0600

    feat : load admin and conditional rendaring of Dashboard sidebar menu items

--- src/Pages/Dashboard/SideBar/SideBar.js
+++ src/Pages/Dashboard/SideBar/SideBar.js
@@ -1,30 +1,48 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../Firebase/Firebase.init";
+import useAdmin from "../../../Hooks/useAdmin";
+import Loading from "../../Shared/Loading/Loading";
 
 const SideBar = () => {
+  const [user] = useAuthState(auth);
+  const [admin, adminLoading] = useAdmin(user);
+  if (adminLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <li>
-        <Link to="/dashboard">My Orders</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/addReview">Add Review</Link>
-      </li>
+      {!admin && (
+        <>
+          <li>
+            <Link to="/dashboard">My Orders</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/addReview">Add Review</Link>
+          </li>
+        </>
+      )}
       <li>
         <Link to="/dashboard/profile">My Profile</Link>
       </li>
-      <li>
-        <Link to="/dashboard/manageOrders">Manage Orders</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/addProducts">Add Product</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/userRole">User Role</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/manageProducts">Manage Products</Link>
-      </li>
+      {admin && (
+        <>
+          <li>
+            <Link to="/dashboard/manageOrders">Manage Orders</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/addProducts">Add Product</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/userRole">User Role</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/manageProducts">Manage Products</Link>
+          </li>
+        </>
+      )}
     </>
   );
 };
