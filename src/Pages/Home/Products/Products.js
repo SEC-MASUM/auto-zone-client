b93bd20c588a981333155255ab0312dda3cce5e0
SEC-MASUM <masum.jess42@gmail.com>
2022-05-27 08:51:30 +0600

    feat : conditional data shown in Home page and add See More button

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {BsFillArrowRightCircleFill} from "react-icons/bs"
 import ProductCard from "../../../Components/ProductCard/ProductCard";
 import useProducts from "../../../Hooks/useProducts";
 import Loading from "../../Shared/Loading/Loading";
@@ -10,17 +12,41 @@ const Products = () => {
   }
   return (
     <div className="container mx-auto my-28">
-      <h1 className="text-4xl font-bold my-12">OUR PRODUCTS</h1>
+      <h1 className="text-4xl text-center  font-bold my-12">OUR PRODUCTS</h1>
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 gap-y-10">
-        {products.data.map((product, index) => (
-          <ProductCard
-            key={product._id}
-            index={index}
-            product={product}
-            refetch={refetch}
-          />
-        ))}
+        {products.data.length > 6
+          ? products.data
+              .slice(0, 6)
+              .map((product, index) => (
+                <ProductCard
+                  key={product._id}
+                  index={index}
+                  product={product}
+                  refetch={refetch}
+                />
+              ))
+          : products.data.map((product, index) => (
+              <ProductCard
+                key={product._id}
+                index={index}
+                product={product}
+                refetch={refetch}
+              />
+            ))}
       </div>
+      {products.data.length > 6 && (
+        <p className="flex items-center justify-end my-6 px-3">
+          <Link
+            to="/products"
+            className=" space-x-2 font-bold text-primary btn btn-sm btn-neutral hover:btn-primary hover:text-white rounded-full"
+          >
+            <span>See More</span>
+            <span>
+              <BsFillArrowRightCircleFill />
+            </span>
+          </Link>
+        </p>
+      )}
     </div>
   );
 };
