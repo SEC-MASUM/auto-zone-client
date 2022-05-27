240c94736ca02fbce34a5efa17ee09e9415b1822
SEC-MASUM <masum.jess42@gmail.com>
2022-05-27 16:14:10 +0600

    style : add Hero and Footer component

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -3,6 +3,8 @@ import ProductCard from "../../Components/ProductCard/ProductCard";
 import Loading from "../Shared/Loading/Loading";
 import Navbar from "../Shared/Navbar/Navbar";
 import useProducts from "../../Hooks/useProducts";
+import Hero from "../../Components/Hero/Hero";
+import Footer from "../Shared/Footer/Footer";
 
 const Products = () => {
   const { products, isLoading, refetch } = useProducts();
@@ -14,8 +16,8 @@ const Products = () => {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-28">
-        <h1 className="text-4xl font-bold my-12">OUR PRODUCTS</h1>
+      <Hero>Our Products</Hero>
+      <div className="container mx-auto my-12">
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10">
           {products.data.map((product, index) => (
             <ProductCard
@@ -27,6 +29,7 @@ const Products = () => {
           ))}
         </div>
       </div>
+      <Footer/>
     </>
   );
 };
